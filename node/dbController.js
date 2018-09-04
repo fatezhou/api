741a91d1d5a5ab89dad1fd75cbae4c6d6fdcf3af
fatezhou <fatezhou@163.com>
2018-09-04 17:43:10 +0800

    commit

--- node/dbController.js
+++ node/dbController.js
@@ -1,19 +1,34 @@
-function CuteDbController(){
-    this.Login = function(data){
+var mysql = require('mysql');
 
-    }
-    this.GetContact = function(data){
 
-    }
-    this.GetProfile = function(data){
+/*
+var connectionz = {
+  host     : 'localhost',
+  user     : 'minidope',
+  password : 'minidope',
+  database : 'minidope'
+};
+*/
 
+function CuteDbController(conn){
+    var connection;
+    this.Init = function(conn){
+        this.connection = mysql.createConnection(conn);
+        this.connection.connect();
     }
-    this.Query = function(sql){
+    this.Query = function(sqlfmt, data, callback){
+        var connection = this.connection;        
+        try{
+            connection.query(sqlfmt, data, function (error, results) {
+                if (error) throw error;
+                callback(results);
+                connection.end();
+            });
+        }catch(err){
 
+        }
     }
-    this.Exec = function(sql){
-        
-    }
+	this.Init(conn);
 }
 
 module.exports = CuteDbController;
---
+++ node/http.js
@@ -0,0 +1,44 @@
+var http = require('http')
+
+function CuteHttp(){
+    this.Request = function(opt){
+        var options = {
+            method: opt.method,
+            host: opt.url,
+            path: opt.path, 
+            port:opt.port,
+            headers: opt.headers || {'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'},
+        }
+        var req = http.request(options, function(res){
+            console.info("Status:" + res.statusCode);
+            res.setEncoding("utf8");
+            res.on("data", function(chuck){
+                if(opt.responseData)
+                    opt.responseData(chuck);
+            });
+            if(opt.response)
+                opt.response(res);
+        });
+
+        req.on("error", opt.requestError);
+        if(opt.method == "POST"){
+            req.write(opt.postData);
+        }
+        req.end();
+    }
+}
+/*
+var obj = new CuteHttp();
+obj.Request({
+    method: "POST",
+    url: "api.minidope.com",
+    port:80,
+    path: "/login",
+    requestError:function(e){console.info(e);},    
+    //response:function(e){console.info(e);},
+    responseData:function(data){console.info("recv:" + data);},
+    postData:"Hello"
+})
+*/
+
+module.exports = CuteHttp;
