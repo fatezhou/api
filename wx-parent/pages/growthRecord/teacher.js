6bfae6e09d331e74bdd385355cfec76d3a8920ed
jianping <1490533119@qq.com>
2018-09-23 23:07:37 +0800

    老师页面跳转

--- wx-parent/pages/growthRecord/teacher.js
+++ wx-parent/pages/growthRecord/teacher.js
@@ -1,66 +1,77 @@
 // pages/growthRecord/teacher.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    teachernickname: '',
+    teachername: '',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    console.info(options.teacherid)
+    console.info(app.globalData.allTeacherInfo)
+    for (var i = 0; i < app.globalData.allTeacherInfo.length; i++) {
+      if (app.globalData.allTeacherInfo[i].teacherId == options.teacherid) {
+        this.setData({
+          teachername: app.globalData.allTeacherInfo[i].name,
+          teachernickname: app.globalData.allTeacherInfo[i].nickname,
+        })
+      }
+    }
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
