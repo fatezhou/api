af71bf45ec00f94f9fccedb617daadf4a2f0be36
fatezhou <fatezhou@163.com>
2018-09-29 21:43:51 +0800

    udpate mongoCtrl.js

--- node/mongoCtrl.js
+++ node/mongoCtrl.js
@@ -7,25 +7,35 @@ function Mongo(){
         if(!mongoUrl){
             mongoUrl = url;
         }
+        if(!table){
+            table = "clientState";
+        }
 
-        MongoClient.connect(mongoUrl, {useNewUrlParser:true}, function(err, db){
-            if(err){
-                callback({error:true, msg:"connect error"});
-            }else{
-                var dbo = db.db("bz");   
-                if(!table){
-                    table = "clientState";
+        function doQuery(db){
+            var dbo = db.db("bz");   
+            dbo.collection(table).find(obj).toArray(function(err, result){
+                if(err){
+                    callback({error:true, msg:"find error"});
+                }else{
+                    db.close();
+                    callback(result);
                 }
-                dbo.collection(table).find(obj).toArray(function(err, result){
-                    if(err){
-                        callback({error:true, msg:"find error"});
-                    }else{
-                        db.close();
-                        callback(result);
-                    }
-                });
-            }
-        })
+            });
+        }
+
+        if(dbconnector){
+            doQuery(dbconnector);
+        }else{
+            MongoClient.connect(mongoUrl, {useNewUrlParser:true}, function(err, db){
+                dbconnector = db;
+                if(err){
+                    callback({error:true, msg:"connect error"});
+                }else{
+                    doQuery(db);
+                }
+            })
+        }
+        
     };
 
     this.Insert = function(obj, callback, mongoUrl, table){
@@ -33,6 +43,21 @@ function Mongo(){
             mongoUrl = url;
         }
 
+        if(!table){
+            table = "clientState";
+        }
+
+        function doInsert(db){
+            var dbo = db.db("bz");
+            dbo.collection(table).insertOne(obj, function(err, res){
+                if(err){
+                    callback({error:true, msg:err});
+                }else{
+                    //db.close();
+                    callback({error:false, msg:"ok"});                        
+                }
+            })
+        }
         
         if(!dbconnector){
             MongoClient.connect(mongoUrl, {useNewUrlParser:true}, function(err, db){
@@ -40,20 +65,11 @@ function Mongo(){
                 if(err){
                     callback({error:true, msg:err});
                 }else{
-                    var dbo = db.db("bz");
-                    if(!table){
-                        table = "clientState";
-                    }
-                    dbo.collection(table).insertOne(obj, function(err, res){
-                        if(err){
-                            callback({error:true, msg:err});
-                        }else{
-                            //db.close();
-                            callback({error:false, msg:"ok"});                        
-                        }
-                    })
+                    doInsert(dbconnector);
                 }
             })
+        }else{
+            doInsert(dbconnector);
         }
 
         
@@ -64,56 +80,95 @@ function Mongo(){
             mongoUrl = url;
         }
 
-        MongoClient.connect(mongoUrl, {useNewUrlParser:true}, function(err, db){
-            if(err){
-                callback({error:true, msg:err});
-            }else{
-                var dbo = db.db("bz");
-                if(!table){
-                    table = "clientState";
+        if(!table){
+            table = "clientState";
+        }
+
+        function doUpdate(db){
+            var dbo = db.db("bz");
+            console.info(mongoUrl, table);
+            dbo.collection(table).updateMany(conditionObj, {$set: commitObj}, function(err, res){
+                if(err){
+                    callback({error:true, count:0, msg:err});
+                }else{
+                    db.close();
+                    callback({error:false, count:res.result});                        
                 }
-                console.info(mongoUrl, table);
-                dbo.collection(table).updateMany(conditionObj, {$set: commitObj}, function(err, res){
-                    if(err){
-                        callback({error:true, count:0, msg:err});
-                    }else{
-                        db.close();
-                        callback({error:false, count:res.result});                        
-                    }
-                })
-            }
-        })
+            })
+        }
+
+        if(dbconnector){
+            doUpdate(dbconnector);
+        }else{
+            MongoClient.connect(mongoUrl, {useNewUrlParser:true}, function(err, db){
+                if(err){
+                    callback({error:true, msg:err});
+                }else{
+                    dbconnector = db;
+                    doUpdate(db);
+                }
+            })
+        }        
     };
 
     this.Delete = function(obj, callback, mongoUrl, table){
         if(!mongoUrl){
             mongoUrl = url;
         }
-        MongoClient.connect(mongoUrl, {useNewUrlParser:true}, function(err, db){
-            if(err){
-                callback({error:true, msg:err});
-            }else{
-                var dbo = db.db("bz");
-                if(!table){
-                    table = "clientState";
+        
+        if(!table){
+            table = "clientState";
+        }
+
+        function doDelete(db){
+            var dbo = db.db("bz");
+            dbo.collection(table).deleteMany(obj, function(err, res){
+                if(err){
+                    callback({error:true, msg:err});                        
+                }else{
+                    db.close();
+                    callback({error: false, msg:res.result.n});
                 }
-                dbo.collection(table).deleteMany(obj, function(err, res){
-                    if(err){
-                        callback({error:true, msg:err});                        
-                    }else{
-                        db.close();
-                        callback({error: false, msg:res.result.n});
-                    }
-                })
-            }
-        });
+            })
+        }
+
+        if(dbconnector){
+            doDelete(dbconnector);
+        }else{
+            MongoClient.connect(mongoUrl, {useNewUrlParser:true}, function(err, db){            
+                if(err){
+                    callback({error:true, msg:err});
+                }else{
+                    dbconnector = db;
+                    doDelete(db);
+                }
+            });
+        }        
+    };
+
+    this.Close = function(){
+        if(dbconnector){
+            db.close();
+        }
     }
 }
 
-// var mgdb = new Mongo;
-// mgdb.Insert({id:1, state:"online"}, function(e){
-//     console.info(e);
-// })
+var mgdb = new Mongo;
+mgdb.Insert({id:1, state:"online"}, function(e){
+    console.info(e);
+})
+mgdb.Insert({id:1, state:"online"}, function(e){
+    console.info(e);
+})
+mgdb.Insert({id:1, state:"online"}, function(e){
+    console.info(e);
+})
+mgdb.Insert({id:1, state:"online"}, function(e){
+    console.info(e);
+})
+mgdb.Insert({id:1, state:"online"}, function(e){
+    console.info(e);
+})
 
 // // mgdb.Query({id:1}, function(e){
 // //     console.info(e);
