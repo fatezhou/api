bf6edb08ad466f00edd28623301191f1f556b8a9
jianping <1490533119@qq.com>
2019-03-08 20:24:12 +0800

    api4 -> oneday

--- wxapp/siteinfo.js
+++ wxapp/siteinfo.js
@@ -4,7 +4,7 @@ var siteinfo = {
     acid: "2",
     multiid: "0",
     version: "8.3.1",
-  siteroot: "https://api4.minidope.com/app/index.php",
+  siteroot: "https://oneday.minidope.com/app/index.php",
     design_method: "3"
 };
 
