b7c1bd519f14254a830e8653f691f476c212d407
mimiddi <1490533119@qq.com>
2019-11-14 15:13:03 +0800

    1

--- wxapp/zh_jdgjb/pages/photography_list/photography_info.js
+++ wxapp/zh_jdgjb/pages/photography_list/photography_info.js
@@ -13,10 +13,60 @@ Page({
     enablezoom: false,
 
     singleroom: [],
+
+    coordinatesList:[{
+      name: 'Oneday Jolly',
+      coordinates: '24.438510,118.087326',
+      address: '厦门市思明区大学路沙坡尾20-2',
+      id: [146, 147, 153]
+    }, {
+      name: 'Oneday大理',
+      coordinates: '25.605676,100.251998',
+      address: '大理白族自治州下关机场路滨海大道洱海天域别墅区C75',
+      id: [143, 144]
+    }, {
+      name: 'Oneday岛屿',
+      coordinates: '24.424934,118.131153',
+      address: '厦门市思明区天泉东里23号',
+      id: [121, 122]
+    }, {
+      name: 'Oneday海眠',
+      coordinates: '24.425970,118.129240',
+      address: '厦门市思明区天泉西里21号',
+      id: [123]
+    }, {
+      name: 'Oneday薄荷',
+      coordinates: '24.437365,118.124252',
+      address: '厦门市思明区龙虎',
+      id: [124, 125]
+    }, {
+      name: 'Oneday北岛',
+      coordinates: '38.881490,121.561338',
+      address: '大连市沙河口区富华街30号oneday北岛',
+      id: [126, 127]
+    }, {
+      name: 'Oneday up on air',
+      coordinates: '22.990424,113.297671',
+      address: '广州市番禺区谢村站地铁口旁',
+      id: [137]
+    }],
   },
   onLoad: function (a) {
     console.info(a)
     var t = this;
+    // 魔改start
+    a.coordinates = function(){
+      let y = t.data.coordinatesList.filter(function (cres) {
+        for (let i in cres.id) {
+          if (cres.id[i] == a.room_id) {
+            return true
+          }
+        }
+        return false
+      })
+      return y.length > 0 ? y[0].coordinates : a.coordinates
+    }()
+    // 魔改end
     app.getUrl(t), app.getSystem(t);
     a.hotel_id;
     var e = a.room_id, n = (new Date().toLocaleDateString().replace(/\//g, "-"), a.coordinates), o = a.tel, i = a.address, r = a.name, s = a.price, yjcost = a.yjcost, latitude = Number(n.split(",")[0]), longitude = Number(n.split(",")[1]);
