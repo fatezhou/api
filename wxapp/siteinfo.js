7e1b607c3369e16147ff28b0acdb15c7a06e6fe1
mimiddi <1490533119@qq.com>
2020-07-13 09:36:20 +0800

    域名更改

--- wxapp/siteinfo.js
+++ wxapp/siteinfo.js
@@ -4,7 +4,7 @@ var siteinfo = {
     acid: "2",
     multiid: "0",
     version: "8.3.1",
-  siteroot: "https://oneday.minidope.com/app/index.php",
+  siteroot: "https://oneday.youyueworld.com/app/index.php",
     design_method: "3"
 };
 
